efec86490ffcd63e5e9aa73edbc673ae35911921
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-07-28 12:28:31 -0400

    added platformio code :crocodile:

--- ultrasonic-flow-rate/src/main.cpp
+++ ultrasonic-flow-rate/src/main.cpp
@@ -1,25 +1,22 @@
-int trigger_pin = 5;
+#include <Arduino.h>
+
+int trigger_pin = 2;
 int echo_pin = 3;
-int testTrigger = 2;
 int time;
 float distance; 
 
 void setup ( ) {
   Serial.begin (9600); 
   pinMode (trigger_pin, OUTPUT); 
-  pinMode (testTrigger, OUTPUT);
   pinMode (echo_pin, INPUT);
-  pinMode (LED_BUILTIN, OUTPUT);
 }
 
 void loop ( ) {
 
   // put out a 10 microsecond pulse
   digitalWrite (trigger_pin, HIGH);
-  digitalWrite (testTrigger, HIGH);
   delayMicroseconds (10);
   digitalWrite (trigger_pin, LOW);
-  digitalWrite (testTrigger, LOW);
 
   // read the time it takes
   time = pulseIn (echo_pin, HIGH);
